8b42dd03043c1e94605a5b9783a9df94564a5ef2
aparnagosula <aparnagosula97@gmail.com>
2021-05-30 01:12:15 +0530

    logging is added

--- src/main/java/com/uhg/kvijay9/IPAddressController.java
+++ src/main/java/com/uhg/kvijay9/IPAddressController.java
@@ -1,5 +1,7 @@
 package com.uhg.kvijay9;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +12,24 @@ import java.net.UnknownHostException;
 @RequestMapping("/ipadrs")
 @RestController
 public class IPAddressController {
+    private final Logger logger = LoggerFactory.getLogger(IPAddressController.class);
 
     @GetMapping
     public String getIPAddress(){
         InetAddress ip = null;
-        /*try {
-        gcloud container clusters get-credentials kvijay9-cluster-1  --zone us-central1-c
+        try {
+            ip = InetAddress.getLocalHost();
+        } catch ( UnknownHostException  e) {
+            e.printStackTrace();
+        }
+        logger.info("IpAddress : " + ip.toString());
+        return ip.toString();
+        /*gcloud container clusters get-credentials kvijay9-cluster-1  --zone us-central1-c
 
         docker run -ti --rm -p 8080:8080 gcr.io/robust-form-315110/ip-service@sha256:e6ae35c0ff5db8068e5544ddcf3d9f18c88d0a8e1ac8ab2844ff36546b2421d1
 
         kubectl run ip-service --image=gcr.io/robust-form-315110/ip-service@sha256:e6ae35c0ff5db8068e5544ddcf3d9f18c88d0a8e1ac8ab2844ff36546b2421d1 --port=8080
-
-            ip = InetAddress.getLocalHost();
-        } catch ( UnknownHostException  e) {
-            e.printStackTrace();
-        }*/
-        return ip.toString();
+        */
     }
 }
 /*
