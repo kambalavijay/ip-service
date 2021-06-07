f948a21f9464a1ca728884846b95c44d5f834596
Vijay Kambala <kmabalavijay@gmail.com>
2021-06-07 18:22:19 +0530

    logging added

--- src/main/java/com/uhg/kvijay9/Feature1Controller.java
+++ src/main/java/com/uhg/kvijay9/Feature1Controller.java
@@ -1,5 +1,7 @@
 package com.uhg.kvijay9;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/feature1")
 public class Feature1Controller {
 
+    Logger logger = LoggerFactory.getLogger(Feature1Controller.class);
+
     @GetMapping
     public String getFeature1(){
+        logger.info("feature1 invoked");
         return "feature1";
     }
+
 }
