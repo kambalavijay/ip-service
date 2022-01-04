9cfd5a766175e2d88cbf62ea4d3e10169c0d058d
Vijay, Kambala <vijay.kambala@optum.com>
2022-01-04 18:36:34 +0530

    improved student get api

--- src/main/java/com/elite/kvijay9/manager/RecipeManager.java
+++ src/main/java/com/elite/kvijay9/manager/RecipeManager.java
@@ -1,10 +1,5 @@
 package com.elite.kvijay9.manager;
 
-import com.elite.kvijay9.model.Recipe;
-
 public class RecipeManager {
-    
-    public static Recipe getRecipe(Class clazz){
-        
-    }
+
 }
